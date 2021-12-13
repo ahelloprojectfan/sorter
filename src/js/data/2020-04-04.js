0ba76ca7d6560bcea2141cef9197567fcd754f1c
ahelloprojectfan <63974154+ahelloprojectfan@users.noreply.github.com>
2021-12-13 07:04:13 -0500

    21

--- src/js/data/2020-04-04.js
+++ src/js/data/2020-04-04.js
@@ -1252,7 +1252,7 @@ dataSet[dataSetVersion].options = [
         tooltip: 'Check this to restrict to certain group',
         checked: true,
         sub: [
-            { name: "Morning Musume '20", key: 'mm' },
+            { name: "Morning Musume '21", key: 'mm' },
             { name: 'Juice=Juice', key: 'jj' },
             { name: 'Angerme', key: 'angerme' },
             { name: 'Tsubaki Factory', key: 'tf' },
