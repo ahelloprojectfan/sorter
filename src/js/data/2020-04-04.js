23aa17817bec8a9256ea8f276cdb7f94f08c241d
chuzzlecheese <96001945+chuzzlecheese@users.noreply.github.com>
2022-07-10 15:33:23 +0100

    en new member pfp

--- src/js/data/2020-04-04.js
+++ src/js/data/2020-04-04.js
@@ -540,7 +540,7 @@ dataSet[dataSetVersion].characterData = [
     },
     {
         "name": "Ishiyama Sakura",
-        "img": "https://i.imgur.com/J8byDtO.jpg"
+        "img": "https://i.imgur.com/rVHiLuf.jpg"
         , opts: {
             subkeyaki: ["jj"]
         }
@@ -1261,14 +1261,14 @@ dataSet[dataSetVersion].characterData = [
     },
     {
         "name": "Sakurai Rio",
-        "img": "https://i.imgur.com/Wdjos6g.jpg"
+        "img": "https://i.imgur.com/zhhpb15.jpg"
         , opts: {
             subkeyaki: ["mm"]
     }
     },
         {
         "name": "Endo Akari",
-        "img": "https://i.imgur.com/AN3jkHB.jpg"
+        "img": "https://i.imgur.com/kdhoqav.jpg"
         , opts: {
             subkeyaki: ["jj"]
         }
