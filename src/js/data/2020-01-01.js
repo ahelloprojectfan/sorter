b3cbca7354e1b5f9dd79c3f7d6a66aa32a178d42
chuzzlecheese <96001945+chuzzlecheese@users.noreply.github.com>
2023-06-22 00:38:29 +0100

    Update 2020-01-01.js

--- src/js/data/2020-01-01.js
+++ src/js/data/2020-01-01.js
@@ -109,7 +109,7 @@ dataSet[dataSetVersion].characterData = [
         "name": "竹内朱莉",
         "img": "https://i.imgur.com/lo0Ybj3.jpg"
         , opts: {
-            subkeyaki: ["angerme"]
+            subkeyaki: ["SnAog"]
         }
     },
     {
