bc99d04e7e92fa6dc6dc85d1fca19f2f26e15dbc
chuzzlecheese <96001945+chuzzlecheese@users.noreply.github.com>
2026-05-29 16:24:58 +0100

    Update 2020-04-04.js

--- src/js/data/2020-04-04.js
+++ src/js/data/2020-04-04.js
@@ -69,6 +69,13 @@ dataSet[dataSetVersion].characterData = [
             subkeyaki: ["mm"]
         }
     },
+			 {
+        "name": "Ishikawa Hanano",
+        "img": "https://static.wikia.nocookie.net/helloproject/images/8/8d/Portal_Ishikawa_Hanano.jpg?1"
+        , opts: {
+            subkeyaki: ["mm"]
+        }
+    },
 
     // Angerme
 
@@ -502,6 +509,13 @@ dataSet[dataSetVersion].characterData = [
             subkeyaki: ["by"]
         }
     },
+	    {
+        "name": "Kojima Hana",
+        "img": "https://static.wikia.nocookie.net/helloproject/images/e/ec/Portal_Kojima_Hana.jpg"
+        , opts: {
+            subkeyaki: ["by"]
+        }
+    },
 	
 	//OCHA NORMA
 	
@@ -680,13 +694,6 @@ dataSet[dataSetVersion].characterData = [
 		 {
         "name": "Suzuki Moa",
         "img": "https://static.wikia.nocookie.net/helloproject/images/b/b3/Portal_Suzuki_Moa.jpg?1"
-        , opts: {
-            subkeyaki: ["kss"]
-        }
-    },
-		 {
-        "name": "Ishikawa Hanano",
-        "img": "https://static.wikia.nocookie.net/helloproject/images/8/8d/Portal_Ishikawa_Hanano.jpg?1"
         , opts: {
             subkeyaki: ["kss"]
         }
